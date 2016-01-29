4d02b983dbc4a278a26fc149a485370f2536dd4e
Ben Simondet <Ben Simondet>
2016-01-28 20:27:26 -0600

    Framework for app created

---
+++ Assets/StartAppButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartAppButton : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        Application.LoadLevel("main");
+    }
+
+}
---
+++ Assets/mainGui.cs
@@ -0,0 +1,46 @@
+﻿//using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.EventSystems;
+//using System.Collections;
+//using System.Collections.Generic;
+
+//public class SampleButton : MonoBehaviour
+//{
+
+//    public Button button;
+//    public Text nameLabel;
+//    public Image icon;
+//    public Text typeLabel;
+//    public Text rarityLabel;
+//    public GameObject championIcon;
+//}
+
+//public class mainGui : MonoBehaviour
+//{
+//    public GameObject sampleButton;
+//    public List<string> wordList;
+
+//    public Transform contentPanel;
+
+//    void Start()
+//    {
+//        PopulateList();
+//    }
+
+//    void PopulateList()
+//    {
+//        for (int i = 0; i < 100; i++)
+//        {
+//            wordList.Add("cat");
+//        }
+
+//        foreach (var item in wordList)
+//        {
+//            GameObject newButton = Instantiate(sampleButton) as GameObject;
+//            SampleButton button = newButton.GetComponent<SampleButton>();
+//            button.nameLabel.text = item;
+//            //button.button.onClick = item.thingToDo;
+//            newButton.transform.SetParent(contentPanel);
+//        }
+//    }
+//}
